191e3651665ea39598468451d66b350208d8d309
justin.maat <jxm262@gmail.com>
2016-10-03 10:05:17 -0400

    adding testing framework

---
+++ test/containers/App.test.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import TestUtils from 'react-addons-test-utils'
+import chai from 'chai'
+import sinon from 'sinon'
+import App from '../../src/containers/App'
+chai.should()
+
+describe("App Container", function() {
+
+    before('render component', function() {
+        // var renderedComponent = TestUtils.renderIntoDocument(
+        //     <TodoItem done={false} name="Write Tutorial"/>
+        // );
+    });
+
+
+    it("does somthing", function() {
+
+
+
+        console.log("asdfasf");
+    });
+});
