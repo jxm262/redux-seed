86b13eb13f5e38729489e6a37f22d93412f65c77
Justin <jxm262@gmail.com>
2017-03-07 22:13:27 -0500

    upgrading to webpack2

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,61 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
+
 
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
+    //entry: [
+    //    'webpack-hot-middleware/client',
+    //    './src/index'
+    //],
     entry: [
-        'webpack-hot-middleware/client',
-        './src/index'
+        'react-hot-loader/patch',
+        // activate HMR for React
+
+        'webpack-dev-server/client?http://localhost:3000',
+
+        'webpack/hot/only-dev-server',
+        // bundle the client for hot reloading
+        // only- means to only hot reload for successful updates
+
+        './src/index.js',
     ],
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+
+        //publicPath: '/static/'
+        //// necessary for HMR to know where to load the hot update chunks
     },
     devServer: {
         contentBase: './src',
         hot: true,
         inline: true,
-        progress: true,
 
         // Display only errors to reduce the amount of output.
         stats: 'errors-only',
 
         host: process.env.HOST || '127.0.0.1',
-        port: process.env.PORT || '8080'
+        port: process.env.PORT || '3000'
     },
     plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        // prints more readable module names in the browser console on HMR updates
+
+        new webpack.NoEmitOnErrorsPlugin(),
+        // do not emit compiled assets that include errors
     ],
     module: {
         loaders: [
             {
                 test: /\.js$/,
-                loaders: ['babel'],
                 exclude: /node_modules/,
-                include: __dirname
+                include: __dirname,
+                use: [
+                    "babel-loader",
+                    "eslint-loader",
+                ],
             },
             {
                 test: /\.css$/, // Only .css files
